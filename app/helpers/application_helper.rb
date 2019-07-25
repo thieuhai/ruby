8d1f26757d3b759c33fb212820dcdb322a66ced1
hai <thieu.hai.113@gmail.com>
2019-07-25 14:40:30 +0700

    Add a Static

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 Module ApplicationHelper
+module ApplicationHelper
   def full_title page_title ="" 
+  locale = params[:locale].to_s.strip.to_sym
+  I18n.locale = I18n.available_locales.include?(locale) ?
+    locale : I18n.default_locale
+end
     base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
       base_title
@@ -7,4 +12,4 @@ Module ApplicationHelper
       page_title + " | " + base_title
     end
   end
-end
+end 
