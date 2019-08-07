acdf58c4bf4dc8f98c7bd2d658922807327b86e8
hai <thieu.hai.113@gmail.com>
2019-08-07 14:05:47 +0700

    cap nhat

--- app/controllers/concerns/user_controllers.rb
+++ app/controllers/concerns/user_controllers.rb
@@ -1,13 +1,10 @@
 class UsersController < ApplicationController
-
   def show
     @user = User.find(params[:id])
   end
-
   def new
     @user = User.new
   end
-
   def create
     @user = User.new(user_params)
     if @user.save
@@ -15,16 +12,13 @@ class UsersController < ApplicationController
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
-      render 'new'
+      render :new
     end
   end
-
   def edit
-    @user = User.find(params[:id])
+    @user = User.find_by(params[:id])
   end
-
   private
-
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
