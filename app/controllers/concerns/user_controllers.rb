79a63f1d5ccc49b918aacdac8e2ba12ac4a0edb8
hai <thieu.hai.113@gmail.com>
2019-08-07 16:21:54 +0700

    chinh

--- app/controllers/concerns/user_controllers.rb
+++ app/controllers/concerns/user_controllers.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.new user_params
     if @user.save
       log_in @user
       flash[:success] = t ".flash.success"
@@ -19,12 +19,6 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find_by(params[:id])
+    @user = User.find_by id: (params[:id])
   end
-
-  private
-    def user_params
-      params.require(:user).permit:name, :email, :password,
-                                   :password_confirmation
-   end
 end
